3fac2841096a15e181dfc75fa4b070bc096742cd
bluebottle <palli@idega.com>
2012-03-08 13:33:33 +0000

    Fixed service so now island.is login works

--- src/java/is/idega/webservice/business/IslandDotIsService.java
+++ src/java/is/idega/webservice/business/IslandDotIsService.java
@@ -4,24 +4,31 @@ import is.skra.kosingar.kodun.KodunLocator;
 import is.skra.kosingar.kodun.Kodun_PortType;
 import is.skra.kosingar.kodun.Status;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import javax.ejb.FinderException;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.rpc.ServiceException;
 import javax.xml.rpc.holders.StringHolder;
 
 import localhost.eGovSAMLGenerator.webServices.generateSAMLFromToken.holders.__StatusHolder;
+import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Elements;
+import nu.xom.ParsingException;
+import nu.xom.ValidityException;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import com.idega.business.IBORuntimeException;
 import com.idega.core.accesscontrol.business.LoginBusinessBean;
@@ -45,7 +52,7 @@ public class IslandDotIsService {
 	private static final String LOGIN_SERVICE_ENDPOINT = "island.is_login_service_endpoint";
 	private static final String LOGIN_SERVICE_USER = "island.is_login_service_user";
 	private static final String LOGIN_SERVICE_PASSWORD = "island.is_login_service_password";
-
+	
 	public boolean createHash(String personalId, String token, String ipAddress) {
 		String endpoint = IWMainApplication
 				.getDefaultIWApplicationContext()
@@ -73,8 +80,7 @@ public class IslandDotIsService {
 			if (status.getCode() == 0) {
 				return true;
 			}
-			
-			
+
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (ServiceException e) {
@@ -112,34 +118,94 @@ public class IslandDotIsService {
 
 			port.generateSAMLFromToken(token, ipAddress, status, saml);
 
-			System.out.println("status.code = " + status.value.getCode());
-			System.out.println("status.message = " + status.value.getMessage());
-			System.out.println("status.type = " + status.value.getType());
-			System.out.println("saml = " + saml.value);
-
-			DocumentBuilder builder = DocumentBuilderFactory.newInstance()
-					.newDocumentBuilder();
-			Document doc = builder.parse(saml.value);
-
-			return "0610703899";
+			if (status.value.getCode() == 0) {
+				Map<String, String> resp = samlInfo(saml.value);
+				String personalId = resp.get("SSN");
+				if (personalId != null) {
+					return personalId;
+				}
+			}			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (ServiceException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
 			e.printStackTrace();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		} 
 
 		return null;
 	}
 
+	public static void main(String args[]) {
+		IslandDotIsService test = new IslandDotIsService();
+		try {
+			String line = null;
+			StringBuilder saml = new StringBuilder();
+			BufferedReader input = new BufferedReader(new FileReader("/Users/palli/response.txt"));
+			while ((line = input.readLine()) != null) {
+				saml.append(line);
+			}
+			Map<String, String> resp = test.samlInfo(saml.toString());
+			Set<String> keys = resp.keySet();
+			for (String key : keys) {
+				String value = resp.get(key);
+				System.out.println(key + " = " + value);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
+	private Map<String, String> samlInfo(String response) {
+
+		Map<String, String> info = new HashMap<String, String>();
+		Builder parser = new Builder();
+		Document docXML = null;
+
+		try {
+			docXML = parser.build( response, "" );
+			insertAttributesInMap( info, docXML.getRootElement() );
+		}
+		catch( ValidityException e ) {
+			e.printStackTrace();
+		}
+		catch( ParsingException e ) {
+			e.printStackTrace();
+		}
+		catch( IOException e ) {
+			e.printStackTrace();
+		}
+
+		return info;
+	}
+	
+	private Element firstChild( Element parent, String name, String namespace ) {
+		Element childElement = parent.getFirstChildElement( name );
+
+		if( (childElement == null) && (namespace != null) ) {
+			childElement = parent.getFirstChildElement( name, namespace );
+		}
+
+		return childElement;
+	}
+	private void insertAttributesInMap( Map<String, String> info, Element assertion ) {
+		Element AttributeStatement = firstChild( assertion, "AttributeStatement", "urn:oasis:names:tc:SAML:1.0:assertion" );
+		Elements attributes = AttributeStatement.getChildElements();
+
+		// Insert all the attributes, from the Attributestatement tag, into the info dictionary
+		for( int i = 0; i < attributes.size(); i++ ) {
+			Element child = attributes.get( i );
+
+			if( child.getLocalName().equals( "Attribute" ) ) {
+				String key = child.getAttribute( "AttributeName" ).getValue();
+				String val = child.getChildElements().get( 0 ).getValue();
+				info.put( key, val );
+			}
+		}
+	}
 	public User authenticateUser(String username, String password)
 			throws java.rmi.RemoteException {
 		try {
