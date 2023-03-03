ca69b5ac33bd0c9aa58aaca3e85586eb9ceb971c
Valdas Žemaitis <valdas@idega.com>
2023-03-03 12:18:28 +0200

    Changed test client

--- src/java/is/idega/webservice/propertyregistryservice/TestClient2.java
+++ src/java/is/idega/webservice/propertyregistryservice/TestClient2.java
@@ -1,17 +1,21 @@
 package is.idega.webservice.propertyregistryservice;
 
-import is.idega.webservice.propertyregistryservice.client.GetDataResponseGetDataResult;
-import is.idega.webservice.propertyregistryservice.client.MainLocator;
-import is.idega.webservice.propertyregistryservice.client.MainSoap_PortType;
-import is.idega.webservice.propertyregistryservice.data.Property;
-
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.axis.message.MessageElement;
+
+import com.idega.util.IWTimestamp;
+
+import is.idega.webservice.propertyregistryservice.client.GetDataResponseGetDataResult;
+import is.idega.webservice.propertyregistryservice.client.MainLocator;
+import is.idega.webservice.propertyregistryservice.client.MainSoap_PortType;
+import is.idega.webservice.propertyregistryservice.data.Property;
 
 public class TestClient2 {
 
@@ -26,7 +30,81 @@ public class TestClient2 {
 	 */
 	public static void main(String[] args) {
 		TestClient2 test = new TestClient2();
-		test.doStuff();
+//		test.doStuff();
+		String year = String.valueOf(IWTimestamp.RightNow().getYear());
+										//	2503562939		2112775219			2106533259
+		String[] addresses = new String[] {"Goðheimar 9",	"Laufásvegur 22A", "Skólavörðustígur 16A"};
+		for (String address: addresses) {
+			List<Property> properties = test.getAppartmentNumbers(address, year, false);
+			System.out.println("Properties for " + address + ": " + properties);
+		}
+	}
+
+	private List<Property> getAppartmentNumbers(String address, String assessmentYear, boolean altered) {
+		try {
+			String endpoint = "https://webservices.fasteignaskra.is/main.asmx";
+
+			MainLocator locator = new MainLocator();
+			MainSoap_PortType port = locator.getMainSoap(new URL(endpoint));
+
+			String companies = port.getCompanies();
+
+			StringTokenizer tok = new StringTokenizer(companies, ";");
+			String company = "";
+			while (tok.hasMoreElements()) {
+				company = (String) tok.nextElement();
+				if (company.length() > 10) {
+					if (company.substring(0, 7).equals("Reykjav")) {
+						break;
+					}
+				}
+			}
+
+			String session = port.login(company, "wsreykjavik", "99Ert");
+
+			StringBuffer filter = new StringBuffer("meini_alagar = '");
+			filter.append(assessmentYear);
+			filter.append("' and meini_heimilisfang='");
+			filter.append(address);
+			filter.append("'");
+
+			GetDataResponseGetDataResult result = port.getData(session,
+					"meini", filter.toString(), false);
+
+			MessageElement properties[] = result.get_any();
+
+			List<Property>apartmentNumbers = new ArrayList<Property>();
+
+			int length = properties.length;
+			for (int i = 0; i < length; i++) {
+				List <Property>propertyList = parseProperty(properties[i]);
+				if (propertyList != null && !propertyList.isEmpty()) {
+					for (Property property : propertyList) {
+						apartmentNumbers.add(property);
+					}
+				}
+			}
+
+			if (apartmentNumbers.isEmpty() && !altered) {
+				String allButLast = address.substring(0, address.length() - 1);
+				String last = address.substring(address.length() - 1);
+
+				if (!last.matches("[0-9]") ) {
+					StringBuffer newAddress = new StringBuffer();
+					newAddress.append(allButLast);
+					newAddress.append(last.toUpperCase());
+
+					return getAppartmentNumbers(newAddress.toString(), assessmentYear, true);
+				}
+			}
+
+			System.out.println("Numbers: " + apartmentNumbers);
+			return apartmentNumbers;
+		} catch (Exception e) {
+			Logger.getLogger(TestClient2.class.getName()).log(Level.WARNING, "Error getting ap. number for " + address, e);
+		}
+
+		return null;
 	}
 
 	private void doStuff() {
@@ -49,31 +127,73 @@ public class TestClient2 {
 				}
 			}
 
-			String session = port.login(company, "idegapalli", "58Rfv");
+			String address = "Laufásvegur 22A";
+			String assessmentYear = String.valueOf(IWTimestamp.RightNow().getYear());
+
+			String session = port.login(company, "wsreykjavik", "99Ert");
+
+			StringBuffer filter = new StringBuffer("meini_alagar = '");
+			filter.append(assessmentYear);
+			filter.append("' and meini_heimilisfang='");
+			filter.append(address);
+			filter.append("'");
 
 			GetDataResponseGetDataResult result = port.getData(session,
-					"meini", "meini_alagar = '2009' and meini_heimilisfang='"
-							+ "Laufásvegur 18A" + "'", false);
+					"meini", filter.toString(), false);
 
-			
 			MessageElement properties[] = result.get_any();
+
+			List<Property>apartmentNumbers = new ArrayList<Property>();
 
 			int length = properties.length;
 			for (int i = 0; i < length; i++) {
-				System.out.println(properties[i].getAsString());
 				List <Property>propertyList = parseProperty(properties[i]);
 				if (propertyList != null && !propertyList.isEmpty()) {
 					for (Property property : propertyList) {
-						System.out.println("apartmentNumber = " + property.getApartmentNumber());
-						System.out.println("fnr = " + property.getFNR());
-						System.out.println("nameNumber = " + property.getNameNumber());
+						apartmentNumbers.add(property);
 					}
 				}
 			}
+
+		if (apartmentNumbers.isEmpty()) {
+			String allButLast = address.substring(0, address.length() - 1);
+			String last = address.substring(address.length() - 1);
+
+			if (!last.matches("[0-9]")) {
+				StringBuffer newAddress = new StringBuffer();
+				newAddress.append(allButLast);
+				newAddress.append(last.toUpperCase());
+
+//				return getApartmentNumberList(newAddress.toString(), assessmentYear, true);
+			}
+		}
+
+		System.out.println("Numbers: " + apartmentNumbers);
+//
+//
+//
+//			GetDataResponseGetDataResult result = port.getData(session,
+//					"meini", "meini_alagar = '2009' and meini_heimilisfang='"
+//							+ "Laufásvegur 18A" + "'", false);
+//
+//
+//			MessageElement properties[] = result.get_any();
+//
+//			int length = properties.length;
+//			for (int i = 0; i < length; i++) {
+//				System.out.println(properties[i].getAsString());
+//				List <Property>propertyList = parseProperty(properties[i]);
+//				if (propertyList != null && !propertyList.isEmpty()) {
+//					for (Property property : propertyList) {
+//						System.out.println("apartmentNumber = " + property.getApartmentNumber());
+//						System.out.println("fnr = " + property.getFNR());
+//						System.out.println("nameNumber = " + property.getNameNumber());
+//					}
+//				}
+//			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	private List<Property> parseProperty(MessageElement element) {
@@ -131,5 +251,5 @@ public class TestClient2 {
 		}
 
 		return;
-	} 
+	}
 }
